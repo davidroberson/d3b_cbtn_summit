e51b49f9c26105496a2ea477ab80c3e0e83a177e
aadamk <adamkraya@d3b.center>
2024-09-20 13:05:25 -0400

    Update run_analysis.sh

--- analyses/methylation_analysis/run_analysis.sh
+++ analyses/methylation_analysis/run_analysis.sh
@@ -37,40 +37,20 @@ Rscript --vanilla 02-dms_gsameth_analysis.R \
 --plots_dir "plots/dms_gsameth_output/reactome"
 
 # using HALLMARK
-Rscript --vanilla 02-dms_gsameth_analysis.R \
---methyl_mat $methyl_m_file \
---diffexpr_sites "results/limma_output/genebody_promoter_diffexpr_probes_per_cluster.tsv" \
---msigdb "hallmark" \
---prefix "genebody_promoter" \
---output_dir "results/dms_gsameth_output/hallmark" \
---plots_dir "plots/dms_gsameth_output/hallmark"
+# Rscript --vanilla 02-dms_gsameth_analysis.R \
+# --methyl_mat $methyl_m_file \
+# --diffexpr_sites "results/limma_output/genebody_promoter_diffexpr_probes_per_cluster.tsv" \
+# --msigdb "hallmark" \
+# --prefix "genebody_promoter" \
+# --output_dir "results/dms_gsameth_output/hallmark" \
+# --plots_dir "plots/dms_gsameth_output/hallmark"
 
 # 2) methreg analysis on differentially methylated CpG sites from (genebody + promoter) obtained from 01-limma_analysis.R
-Rscript --vanilla 02-methreg_analysis.R \
---methyl_mat $methyl_m_file \
---cluster_file $cluster_file \
---expr_mat $count_file \
---diffexpr_sites "results/limma_output/genebody_promoter_diffexpr_probes_per_cluster.tsv" \
---output_dir "results/methreg_output" \
---plots_dir "plots/methreg_output"
-
-# 3) identify differentially methylation regions using DMRcate::dmrcate and pathway enrichment using missMethyl::gsaregion
-# using REACTOME
-Rscript --vanilla 03-dmr_gsaregion_analysis.R \
---methyl_mat $methyl_m_file \
---methyl_annot $methyl_annot_file \
---cluster_file $cluster_file \
---msigdb "reactome" \
---output_dir "results/dmr_gsaregion_output/reactome" \
---plots_dir "plots/dmr_gsaregion_output/reactome"
-
-# using HALLMARK
-Rscript --vanilla 03-dmr_gsaregion_analysis.R \
---methyl_mat $methyl_m_file \
---methyl_annot $methyl_annot_file \
---cluster_file $cluster_file \
---msigdb "hallmark" \
---output_dir "results/dmr_gsaregion_output/hallmark" \
---plots_dir "plots/dmr_gsaregion_output/hallmark"
-
+# Rscript --vanilla 02-methreg_analysis.R \
+# --methyl_mat $methyl_m_file \
+# --cluster_file $cluster_file \
+# --expr_mat $count_file \
+# --diffexpr_sites "results/limma_output/genebody_promoter_diffexpr_probes_per_cluster.tsv" \
+# --output_dir "results/methreg_output" \
+# --plots_dir "plots/methreg_output"
 
