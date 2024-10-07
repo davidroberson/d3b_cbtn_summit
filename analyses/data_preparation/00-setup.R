945c0c74f9956f77340bf4c349c636d6828dbfaa
komalsrathi <komal.rathi@gmail.com>
2024-10-07 09:03:43 -0400

    add repos param

--- analyses/data_preparation/00-setup.R
+++ analyses/data_preparation/00-setup.R
@@ -2,20 +2,20 @@
 
 # CRAN packages
 if (!require("optparse", quietly = TRUE))
-  install.packages("optparse")
+  install.packages("optparse", repos = "http://cran.us.r-project.org")
 
 if (!require("tidyverse", quietly = TRUE))
-  install.packages("tidyverse")
+  install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 
 if (!require("datawizard", quietly = TRUE))
-  install.packages("datawizard")
+  install.packages("datawizard", repos = "http://cran.us.r-project.org")
 
 if (!require("reshape2", quietly = TRUE))
-  install.packages("reshape2")
+  install.packages("reshape2", repos = "http://cran.us.r-project.org")
 
 # Bioc packages
 if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+  install.packages("BiocManager", repos = "http://cran.us.r-project.org")
 
 if (!require("rtracklayer", quietly = TRUE))
   BiocManager::install("rtracklayer")
